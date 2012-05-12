1150f2c1da54bfd4e2f09dafb44a7a82967c9693
Dan Burton <danburton.email@gmail.com>
2012-05-11 18:16:37 -0600

    Attempted to use Cont. Runs but doesn't quite work (?).

--- Parser.hs
+++ Parser.hs
@@ -1,56 +1,49 @@
-{-# LANGUAGE EmptyDataDecls, KindSignatures #-}
-{-# OPTIONS_GHC -fno-warn-missing-methods #-}
-
 module Parser (toProgram) where
 
 import TapeM
 import Program
 
 import Control.Applicative
+import Control.Monad.Cont
 
 
 type TapeP = Program TapeM
-
-data MagicMonad :: * -> *
-instance Monad MagicMonad
-instance Applicative MagicMonad
-instance Functor MagicMonad
-
-runMagicMonad :: MagicMonad a -> a
-runMagicMonad = undefined
+type TapeC = Cont TapeP
 
 toProgram :: String -> TapeP
-toProgram = runMagicMonad . toProgramStep
+toProgram = (`runCont` id) . toProgramStep
 
-data MagicToken :: *
 
-pushBegin :: MagicMonad TapeP -> MagicMonad MagicToken
-pushBegin = undefined
+push :: TapeC TapeP -> TapeC TapeP
+push mcontinue = do
+  continue <- mcontinue
+{-  cont (\breakMake -> let break = breakMake me
+                          me = loopControl continue break
+                      in me) -}
+  cont (\breakMake -> loopControl continue (breakMake continue))
 
-pushEnd   :: MagicMonad TapeP -> MagicToken -> MagicMonad TapeP
-pushEnd = undefined
+pop :: TapeC TapeP -> TapeC TapeP
+pop mbreak = do
+  break <- mbreak
+{-  cont (\continueMake -> let continue = continueMake me
+                             me = loopControl continue break
+                         in me) -}
+  cont (\continueMake -> loopControl (continueMake break) break)
 
-popBegin :: MagicMonad MagicToken
-popBegin = undefined
-
-popEnd   :: MagicToken -> MagicMonad TapeP
-popEnd = undefined
-
-liftI :: TapeM () -> String -> MagicMonad TapeP
+liftI :: TapeM () -> String -> TapeC TapeP
 liftI i cs = Instruction i <$> toProgramStep cs
 
--- this seems like it will be useful
 loopControl :: TapeP -> TapeP -> TapeP
 loopControl = branch is0
 
-toProgramStep :: String -> MagicMonad TapeP
+toProgramStep :: String -> TapeC TapeP
 toProgramStep ('>':cs) = liftI right   cs
 toProgramStep ('<':cs) = liftI left    cs
 toProgramStep ('+':cs) = liftI incr    cs
 toProgramStep ('-':cs) = liftI decr    cs
 toProgramStep (',':cs) = liftI inChar  cs
 toProgramStep ('.':cs) = liftI outChar cs
-toProgramStep ('[':cs) = pushBegin (toProgramStep cs) >>= popEnd
-toProgramStep (']':cs) = popBegin >>= pushEnd (toProgramStep cs)
+toProgramStep ('[':cs) = push (toProgramStep cs)
+toProgramStep (']':cs) = pop (toProgramStep cs)
 toProgramStep (_:cs) = toProgramStep cs
 toProgramStep [] = return Halt
