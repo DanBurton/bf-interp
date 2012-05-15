6c40f4d8f2a8d901c98d7b75b434e98d815e9331
Dan Burton <danburton.email@gmail.com>
2012-05-15 11:18:25 -0600

    Using MonadFix with MaybeT and State, output is still erroneous

--- Parser.hs
+++ Parser.hs
@@ -1,49 +1,67 @@
+{-# LANGUAGE DoRec, TypeSynonymInstances, FlexibleInstances #-}
+
 module Parser (toProgram) where
 
 import TapeM
 import Program
 
 import Control.Applicative
-import Control.Monad.Cont
-
+import Control.Monad.State
+import Control.Monad.Trans.Maybe
+import Data.Maybe (fromMaybe)
 
 type TapeP = Program TapeM
-type TapeC = Cont TapeP
+type ParseState = MaybeT (State [TapeP])
 
-toProgram :: String -> TapeP
-toProgram = (`runCont` id) . toProgramStep
+push :: TapeP -> ParseState ()
+push p = lift $ modify (p:)
+
+pop :: ParseState TapeP
+pop = do
+  (x:xs) <- get
+  put xs
+  return x
+
+-- copied from Control.Monad.Trans.Maybe in transformers-0.3.0.0
+instance (MonadFix m) => MonadFix (MaybeT m) where
+  mfix f = MaybeT (mfix (runMaybeT . f . unJust))
+    where unJust = fromMaybe (error "mfix MaybeT: Nothing")
 
 
-push :: TapeC TapeP -> TapeC TapeP
-push mcontinue = do
-  continue <- mcontinue
-{-  cont (\breakMake -> let break = breakMake me
-                          me = loopControl continue break
-                      in me) -}
-  cont (\breakMake -> loopControl continue (breakMake continue))
+toProgram :: String -> Maybe TapeP
+toProgram = flip evalState [] . runMaybeT . toProgramStep
 
-pop :: TapeC TapeP -> TapeC TapeP
-pop mbreak = do
+
+openBrace :: ParseState TapeP -> ParseState TapeP
+openBrace mcontinue = do
+  rec continue <- push loop >> mcontinue
+      loop     <- do
+            break <- pop
+            return $ loopControl continue break
+  return loop
+
+closeBrace :: ParseState TapeP -> ParseState TapeP
+closeBrace mbreak = do
+  loop <- pop
   break <- mbreak
-{-  cont (\continueMake -> let continue = continueMake me
-                             me = loopControl continue break
-                         in me) -}
-  cont (\continueMake -> loopControl (continueMake break) break)
+  push break
+  return loop
 
-liftI :: TapeM () -> String -> TapeC TapeP
+
+liftI :: TapeM () -> String -> ParseState TapeP
 liftI i cs = Instruction i <$> toProgramStep cs
 
 loopControl :: TapeP -> TapeP -> TapeP
 loopControl = branch is0
 
-toProgramStep :: String -> TapeC TapeP
+toProgramStep :: String -> ParseState TapeP
 toProgramStep ('>':cs) = liftI right   cs
 toProgramStep ('<':cs) = liftI left    cs
 toProgramStep ('+':cs) = liftI incr    cs
 toProgramStep ('-':cs) = liftI decr    cs
 toProgramStep (',':cs) = liftI inChar  cs
 toProgramStep ('.':cs) = liftI outChar cs
-toProgramStep ('[':cs) = push (toProgramStep cs)
-toProgramStep (']':cs) = pop (toProgramStep cs)
+toProgramStep ('[':cs) = openBrace (toProgramStep cs)
+toProgramStep (']':cs) = closeBrace  (toProgramStep cs)
 toProgramStep (_:cs) = toProgramStep cs
 toProgramStep [] = return Halt
